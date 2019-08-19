7ebdb1c43c6639d5ee245efb221fc770f8847c8d
DeeFG <xdeishax@gmail.com>
2019-08-19 03:33:21 -0400

    fix sequlize bug

---
+++ models/burger.js
@@ -0,0 +1,8 @@
+
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        name: DataTypes.STRING,
+        devoured_state: DataTypes.STRING
+    });
+    return Burger;
+}
